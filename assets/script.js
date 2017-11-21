53f23aa30c3c0a2131f23fe9136f044b44bd2a4e
cataire <mail.for.mu@gmail.com>
2017-11-20 20:17:33 -0700

    more functions

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,21 @@
   	displayArray: [],
   	guesses: [],
   	computerChoice: function {
-  		wordLibrary[Math.floor(Math.random()*wordArray.length)]},
+  		wordLibrary[Math.floor(Math.random()*wordArray.length)];
+  		console.log(computerChoice);
+  	},
 
   	buildDisplayArray: function {
   		for (var index = 0; i<wordArray.length; index++) {
   			displayArray.push(" _ ");
   		}
+
   		},
-  };
+  	updateScreen: function {
+  		$("#answerDisplay").html(displayArray);
+
+  		}
+  		};
+computerChoice();
+buildDisplayArray();
+updateScreen();
