52abfbb651454d87e65fd95bef268da9697d9b6f
lpeck <lisa84@gmail.com>
2017-11-20 20:24:11 -0700

    Lisa adding counter

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
  var gameLogic = {
 
+    NumGuesses: 15;
   	wins: 0,
   	losses: 0,
   	wordArray: ["kitten", "towel", "flower"],
@@ -11,6 +12,20 @@
   	buildDisplayArray: function {
   		for (var index = 0; i<wordArray.length; index++) {
   			displayArray.push(" _ ");
+
+    onkeypress = function(){
+      var GuessesLeft = NumGuesses--;
+      alert(DecGuessesLeft + " Guesses left")
+
+    };  
   		}
   		},
   };
+
+  var EasyLogic {
+    wordArray: ["dog","cat","fish","bird","cow","pig"];
+    NumGuesses: 30;
+    displayArray: [];
+    guesses: [];
+  }
+
